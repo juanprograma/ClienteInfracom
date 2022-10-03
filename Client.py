2e84d7d31451446a611fe1c77fb64f2e2cf6ac19
LeandroYara <69647862+LeandroYara@users.noreply.github.com>
2022-10-03 18:35:36 -0500

    Envio de archivos con error de lectura

--- Client.py
+++ Client.py
@@ -1,33 +1,50 @@
 import hashlib
+import os
 import socket
 import time
+import tqdm
 
 IP = socket.gethostbyname(socket.gethostname())
 PORT = 5566
 ADDR = (IP, PORT)
-SIZE = 1024
+SIZE = 2048
 FORMAT = "utf-8"
+SEPARATOR = "<SEPARATOR>"
 
 def main():
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    temporalHash = hashlib.sha256()
+    newHash = ""
     client.connect(ADDR)
     print(f"[CONNECTED] Client connected to server at {IP}:{PORT}")
     
     start = time.time()
-    msg = client.recv(SIZE).decode(FORMAT)
-    lmsg = eval(msg)
-    print(lmsg)
-    newHash = hashlib.sha256(lmsg[0].encode()).hexdigest()
-    print (newHash)
-    end = time.time()
-    recievingTime = end - start
-    print(recievingTime)
-    if newHash == lmsg[1]:
-        client.send("File was recieved correctly".encode(FORMAT))
+    received = client.recv(SIZE).decode()
+    filename, filesize, clientNumber, dataHash = received.split(SEPARATOR)
+    print(f"{filename}, {filesize}, {clientNumber}")
+    filename = clientNumber + "-" + filename
+    filesize = int(filesize)
+    progress = tqdm.tqdm(range(filesize), f"Receiving {filename}", unit="B", unit_scale=True, unit_divisor=1024)
+    file = open(filename, "wb")
+    while True:
+        bytes_read = client.recv(SIZE)
+        if not bytes_read:    
+            break
+        file.write(bytes_read)
+        progress.update(len(bytes_read))
+    for chunk in iter(lambda: file.read(SIZE), b''):
+        temporalHash.update(chunk)
+        newHash += temporalHash.hexdigest()
+    if newHash == dataHash:
+        client.send("File was received correctly".encode(FORMAT))
         print("Successful download")
     else:
         client.send("File hash doesn't match with expected hash".encode(FORMAT))
         print("File doesn't match with expected hash")
+        
+    end = time.time()
+    recievingTime = end - start
+    print(str(recievingTime))
 
 if __name__ == "__main__":
     main()
--- Server.py
+++ Server.py
@@ -5,55 +5,69 @@ import threading
 import time
 import os
 
+import tqdm
+
 IP = socket.gethostbyname(socket.gethostname())
 PORT = 5566
 ADDR = (IP, PORT)
-SIZE = 1024
+SIZE = 2048
 FORMAT = "utf-8"
+SEPARATOR = "<SEPARATOR>"
 
 def main():
     print("[STARTING] Server is starting...")
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server.bind(ADDR)
     fileType = int(input("Ingrese 1 para enviar el archivo de 100MB o ingrese 2 para enviar el archivo de 250MB: "))
-    concurrentClients = int(input("ingrese el numero de clientes que desea atender en simultaneo: "))
+    concurrentClients = int(input("Ingrese el numero de clientes que desea atender en simultaneo: "))
     barrier = threading.Barrier(concurrentClients)
+    temporalHash = hashlib.sha256()
+    dataHash = ""
     if fileType == 1:
-        file = open("serverFiles/100MB.bin", "r")
+        file = open("serverFiles/100MB.bin", "rb")
     elif fileType == 2:
-        file = open("serverFiles/250MB.bin", "r")
+        file = open("serverFiles/250MB.bin", "rb")
+    for chunk in iter(lambda: file.read(SIZE), b''):
+        temporalHash.update(chunk)
+        dataHash += temporalHash.hexdigest()
     fileName = os.path.basename(file.name)
     file.seek(0, os.SEEK_END)
     fileSize = file.tell()
-    data = file.read()
+    file.close()
+    print(f"The file {fileName} with {fileSize} Bytes was loaded and its hash was calculated")
+    clientCount = 1
     server.listen()
     print(f"[LISTENING] Server is listening on {IP}:{PORT}")
 
     while True:
         conn, addr = server.accept()
-        thread = threading.Thread(target=handle_client, args=(conn, addr, barrier, data, fileName, fileSize))
+        thread = threading.Thread(target=handle_client, args=(conn, addr, barrier, dataHash, fileName, fileSize, clientCount))
+        clientCount+= 1
         thread.start()
         print(f"[ACTIVE CONNECTIONS] {threading.active_count() - 1}")
 
-def handle_client(conn, addr, barrier, data, fileName, fileSize):
+def handle_client(conn, addr, barrier, dataHash, fileName, fileSize, clientNumber):
     print(f"[NEW CONNECTION] {addr} connected.")
-    
     barrier.wait()
     now = datetime.datetime.now()
     print (now.strftime("%m/%d/%Y, %H:%M:%S"))
     start = time.time()
-    dataHash = hashlib.sha256(data.encode(FORMAT)).hexdigest()
-    print(dataHash)
-    conn.send(str([data, dataHash]).encode(FORMAT))
-    print ([data, dataHash])
-    end = time.time()
-    sendingTime = end - start
-    print (str(sendingTime))
+    conn.send(f"{fileName}{SEPARATOR}{fileSize}{SEPARATOR}{clientNumber}{SEPARATOR}{dataHash}".encode())
+    progress = tqdm.tqdm(range(fileSize), f"Sending {fileName}", unit="B", unit_scale=True, unit_divisor=1024)
+    with open("serverFiles/" + fileName, "rb") as file:
+        while True:
+            bytes_read = file.read(SIZE)
+            if not bytes_read:
+                break
+            conn.sendall(bytes_read)
+            progress.update(len(bytes_read))
     print(f"[{addr}] {fileName}")
     print(f"File sended: {fileName} to client {addr} with size of {fileSize} Bytes")
-     
     msgFinal = conn.recv(SIZE).decode(FORMAT)
     print (f"[{addr}]: {msgFinal}")
+    end = time.time()
+    sendingTime = end - start
+    print(str(sendingTime))
 
     conn.close()
 
